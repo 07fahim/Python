ef166dcbf3473e1e095147731bd6b08fa2ff50ba
Fahim Fiyaz <faiyazfahim743@gmail.com>
2025-01-03 22:08:35 +0600

    Streamlit

--- Python/9-Streamlit/widgets.py
+++ Python/9-Streamlit/widgets.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Streamlit Text Input")
 
 name=st.text_input("Enter your name:")
 if name:
     st.write(f"Hello {name}")
-    
+
 
 age=st.slider("Enter your age:",0,100,25)
 st.write(f"{name} age is {age}")
@@ -13,3 +14,11 @@ st.write(f"{name} age is {age}")
 options=['C','C++','Java','Python','Dart','JavaScript']
 choice=st.selectbox("Choice your favourite language:",options)
 st.write(f"You selected {choice}.")
+
+data = {
+"Name": ["John", "Jane", "Jake", "Jill"],
+"Age": [28, 24, 35, 40],
+"City": ["New York", "Los Angeles", "Chicago", "Houston"]
+}
+df = pd.DataFrame(data)
+st.write(df)
